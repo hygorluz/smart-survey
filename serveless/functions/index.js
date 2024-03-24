02b4a7c8b9fce3a4ae4f84e90c501870069230e7
Guilherme Guimarães Cazaroto <gui.cazaroto@gmail.com>
2024-03-24 11:32:17 -0300

    login endpoint

--- serveless/functions/index.js
+++ serveless/functions/index.js
@@ -1,10 +1,12 @@
-
 // const { onRequest } = require("firebase-functions/v2/https")
 const functions = require('firebase-functions');
 const admin = require("firebase-admin");
 const app = require("express")();
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 
 admin.initializeApp();
+
 const db = admin.firestore().collection("survey");
 
 app.get("/survey", function (request, response) {
@@ -21,6 +23,29 @@ app.get("/survey", function (request, response) {
       response.json(surveyList);
     });
 })
+
+app.post('/login', (req, res) => {
+  const { login, password } = req.body;
+  const passwordHash = crypto.createHash('sha256').update(password).digest('hex');
+
+  db.collection('users').doc(login).get().then((doc) => {
+    if (!doc.exists) {
+      return res.status(404).send('Usuário não encontrado.');
+    }
+
+    const user = doc.data();
+    if (user.password === passwordHash) {
+      const token = jwt.sign({ login }, 'SmartSurvey', { expiresIn: '1h' })
+      res.json({ token });
+    } else {
+      res.status(401).send('Login ou senha incorretos.');
+    }
+  }).catch((error) => {
+    console.error('Erro ao buscar usuário:', error);
+    res.status(500).send('Erro interno do servidor.');
+  });
+});
+
 
 // app.post("/survey", function (request, response) {
 //   db.add({ description: request.body.description })

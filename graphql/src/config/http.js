d4339636587a4322b0e6aee68ed1836eb4a53548
Danilo Augusto <danilo.augusto@artefatodigital.com>
2024-04-07 17:54:44 -0300

    Update Token

--- graphql/src/config/http.js
+++ graphql/src/config/http.js
@@ -1,14 +1,15 @@
 const axios = require("axios");
+const { getToken } = require("./tokenManager");
 
-// import { env } from "./env"
-// const defaultEnv = "production"
+async function createApi() {
+  const token = await getToken();
+  console.log(token);
+  return axios.create({
+    baseURL: "https://us-central1-serveless-survey-api.cloudfunctions.net/api/",
+    headers: {
+      Authorization: `${token}`,
+    },
+  });
+}
 
-const api = axios.create({
-  //baseURL: "https://assem.com.br/wp-json/wp/v2/",
-  baseURL: "https://us-central1-serveless-survey-api.cloudfunctions.net/api/",
-  headers: {
-    Authorization: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dpbiI6ImFkbWluIiwiaWF0IjoxNzEyMTEwNDM4LCJleHAiOjE3MTIxMTQwMzh9.AsS8-lffvaVMf5vswt-c-wNoFiCa0AQca1QvAgHjgr0`,
-  }
-});
-
-module.exports = api;
+module.exports = createApi;
